c4248f242eb17f87e010cc5d104a45afa05b4f4e
ssc255 <ssc255@cornell.edu>
2021-10-23 10:57:20 -0400

    typing for util.py

--- src/util.py
+++ src/util.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn.utils.rnn as rnn
+from typing import List, Tuple
 
-def pad_batch(batch):
+def pad_batch(batch: Tuple[torch.LongTensor, torch.LongTensor]) \
+    -> Tuple[torch.LongTensor, torch.LongTensor, torch.LongTensor]:
   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
   (xs, ys) = zip(*batch)
   x_lens = torch.LongTensor([len(x) for x in xs])
@@ -11,7 +13,7 @@ def pad_batch(batch):
   y_pad = y_pad.to(device)
   return x_pad, x_lens, y_pad
 
-def pad_test_batch(batch):
+def pad_test_batch(batch: torch.LongTensor) -> Tuple[torch.LongTensor, torch.LongTensor]:
   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
   x_lens = torch.LongTensor([len(x) for x in batch])
   x_pad= rnn.pad_sequence([x for x in batch], padding_value=0, batch_first=True)
