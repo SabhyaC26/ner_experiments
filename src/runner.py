2ad22442eb0f67fa7dd342ab99622314dd26aa04
ssc255 <ssc255@cornell.edu>
2021-10-24 22:47:59 -0400

    remove merge confs

--- src/runner.py
+++ src/runner.py
@@ -102,25 +102,17 @@ def main(args):
 
     # mappings + datasets + dataloaders
     train, val, test = load_data()
-    test = test.select(range(100))
+    # test = test.select(range(100))
     ner_tags = train.features['ner_tags'].feature.names
     tokens_to_idx, idx_to_tokens = build_token_mappings(train['tokens'])
     tags_to_idx, idx_to_tags = build_tag_mappings(ner_tags)
     train_data = Conll2003(
-<<<<<<< HEAD
-        tokens=train['tokens'][:100], labels=train['ner_tags'][:100],
-=======
         tokens=train['tokens'], labels=train['ner_tags'],
->>>>>>> 46c18a43fd724e6cf8e752f0aca22bd2909aaffe
         idx_to_tokens=idx_to_tokens, tokens_to_idx=tokens_to_idx,
         tags_to_idx=tags_to_idx, idx_to_tags=idx_to_tags
     )
     val_data = Conll2003(
-<<<<<<< HEAD
-        tokens=val['tokens'][:100], labels=val['ner_tags'][:100],
-=======
         tokens=val['tokens'], labels=val['ner_tags'],
->>>>>>> 46c18a43fd724e6cf8e752f0aca22bd2909aaffe
         idx_to_tokens=idx_to_tokens, tokens_to_idx=tokens_to_idx,
         tags_to_idx=tags_to_idx, idx_to_tags=idx_to_tags
     )
