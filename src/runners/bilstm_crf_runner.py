80c8ef281d678f2497cf6ca0d6c289ca47f6187a
ssc255 <ssc255@cornell.edu>
2021-11-09 20:42:39 -0500

    added support for wandb config

--- src/runners/bilstm_crf_runner.py
+++ src/runners/bilstm_crf_runner.py
@@ -1,6 +1,9 @@
 import argparse
 import time
 
+import torch.optim
+import wandb
+
 import allennlp.modules.conditional_random_field as crf
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -81,9 +84,9 @@ def decode_batch(model, batch: List[torch.LongTensor], idx_to_tags: Dict[int, st
     return actual_pred_tags
 
 
-def main(args):
+def main(args, config, run_id):
     # init logger
-    logger = init_logger(args.out, args.run_id, 'BiLSTM-CRF')
+    logger = init_logger(args.out, run_id, 'BiLSTM-CRF')
     logger.info('BiLSTM-CRF Model on Conll-2003 NER')
 
     # mappings + datasets + dataloaders
@@ -102,13 +105,13 @@ def main(args):
         idx_to_tokens=idx_to_tokens, tokens_to_idx=tokens_to_idx,
         idx_to_tags=idx_to_tags, tags_to_idx=tags_to_idx
     )
-    train_dataloader = DataLoader(dataset=train_data, batch_size=args.batch_size, shuffle=True, collate_fn=pad_batch)
-    val_dataloader = DataLoader(dataset=val_data, batch_size=args.batch_size, shuffle=True, collate_fn=pad_batch)
+    train_dataloader = DataLoader(dataset=train_data, batch_size=config.batch_size, shuffle=True, collate_fn=pad_batch)
+    val_dataloader = DataLoader(dataset=val_data, batch_size=config.batch_size, shuffle=True, collate_fn=pad_batch)
 
     # load glove embeddings
     logger.info('Loading glove embeddings.')
     glove_folder = args.glove
-    glove_embeddings = load_glove_embeddings(glove_folder=glove_folder, embedding_dim=args.embedding_dim,
+    glove_embeddings = load_glove_embeddings(glove_folder=glove_folder, embedding_dim=config.embedding_dim,
                                              init='zeros', tokens_to_idx=tokens_to_idx)
     logger.info('Glove embeddings have been loaded')
 
@@ -120,11 +123,11 @@ def main(args):
     bilstm_crf = BiLSTM_CRF(
         vocab_size=len(train_data.idx_to_tokens.keys()),
         num_tags=len(train_data.idx_to_tags.keys()),
-        embedding_dim=args.embedding_dim,
+        embedding_dim=config.embedding_dim,
         embeddings=glove_embeddings,
-        lstm_hidden_dim=args.hidden_dim,
-        lstm_num_layers=args.num_layers,
-        dropout=args.dropout,
+        lstm_hidden_dim=config.hidden_dim,
+        lstm_num_layers=config.num_layers,
+        dropout=config.dropout,
         constraints=crf_constraints,
         pad_idx=train_data.tokens_to_idx[PAD]
     )
@@ -135,15 +138,15 @@ def main(args):
     logger.info(f'The model has {num_params:,} trainable parameters')
 
     # run model
-    optimizer = torch.optim.Adam(bilstm_crf.parameters())
+    optimizer = torch.optim.SGD(bilstm_crf.parameters(), lr=config.lr, momentum=config.momentum)
     best_val_loss = float('inf')
-    for epoch in range(args.epochs):
+    for epoch in range(config.epochs):
         start_time = time.time()
-        train_loss = train_model(model=bilstm_crf, dataloader=train_dataloader, optimizer=optimizer, clip=args.clip)
+        train_loss = train_model(model=bilstm_crf, dataloader=train_dataloader, optimizer=optimizer, clip=config.clip)
         val_loss = evaluate_model(model=bilstm_crf, dataloader=val_dataloader)
         end_time = time.time()
 
-        predicted_labels = test_model(test_data=test, model=bilstm_crf, batch_size=args.batch_size,
+        predicted_labels = test_model(test_data=test, model=bilstm_crf, batch_size=config.batch_size,
                                       tokens_to_idx=tokens_to_idx, idx_to_tags=idx_to_tags)
 
         gold_labels = []
@@ -154,7 +157,7 @@ def main(args):
 
         if val_loss < best_val_loss:
             best_val_loss = val_loss
-            out_path = os.path.join(args.out, 'weights', f'bilstm_crf_{args.run_id}.pt')
+            out_path = os.path.join(args.out, 'weights', f'bilstm_crf_{run_id}.pt')
             torch.save(bilstm_crf.state_dict(), out_path)
 
         epoch_mins, epoch_secs = calculate_epoch_time(start_time, end_time)
@@ -170,15 +173,20 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Args for BiLSTM_CRF')
     parser.add_argument('--out', help='output directory for logs', required=True, type=str)
-    parser.add_argument('--embedding-dim', help='dimension for embeddings', default=300, type=int)
-    parser.add_argument('--hidden-dim', help='dimension for hidden layer', default=512, type=int)
-    parser.add_argument('--num-layers', help='number of lstm layers', default=1, type=int)
-    parser.add_argument('--dropout', help='regularization parameter', default=0.2, type=float)
-    parser.add_argument('--batch-size', help='train/val batch size', default=64, type=int)
-    parser.add_argument('--clip', help='gradient clipping parameter', default=1, type=int)
-    parser.add_argument('--epochs', help='number of epochs', default=5, type=int)
-    parser.add_argument('--lr', help='learning rate for optimizer', type=float)
     parser.add_argument('--glove', help='path too the folder with glove files', type=str)
-    parser.add_argument('--run-id', help='id for the current run', type=int, required=True)
+
     args = parser.parse_args()
-    main(args)
+    run = wandb.init(reinit=True)
+    wandb.config = {
+        "embedding_dim": 300,
+        "hidden_dim": 512,
+        "num_layers": 1,
+        "dropout": 0.5,
+        "batch_size": 64,
+        "clip": 1,
+        "epochs": 25,
+        "lr": 0.001,
+        "momentum": 0.9
+    }
+    main(args=args, config=wandb.config, run_id=run.id)
+    run.finish()
